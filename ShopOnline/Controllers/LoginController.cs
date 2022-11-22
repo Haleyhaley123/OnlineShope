0ba2ea062c815ee7c49b9477499f455811a6545c
halt63 <halt63@vingroup.net>
2022-11-22 11:28:37 +0700

    update

--- ShopOnline/Controllers/LoginController.cs
+++ ShopOnline/Controllers/LoginController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ShopOnline.Code;
+using ShopOnline.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,13 +11,26 @@ namespace ShopOnline.Controllers
     public class LoginController : Controller
     {
         // GET: Login
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
-        public ActionResult Login()
+        [HttpPost]
+        [ValidateAntiForgeryToken]// server sinh ra token và client có 1 token, 2 token tương ứng với nhau
+        public ActionResult Login(LoginModels model)
         {
-            return View();
+           if(model.Username == "halt63" && model.Password == "1234")
+            {
+                SessionHelper.SetSession(new UserSession() { UserName = model.Username });
+                return RedirectToAction("Index", "Login");
+            }
+            else
+            {
+                ModelState.AddModelError("","Tên đăng nhập hoặc mật khẩu không chính xác");
+            }
+           
+            return View(model);
         }
     }
 }
